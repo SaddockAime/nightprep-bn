03c09e283321dd307939069c282508c780b2187b
Saddock Kabandana <aimegetz@gmail.com>
2025-08-07 16:19:40 +0200

    fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 // Get current user
 exports.getMe = async (req, res) => {
